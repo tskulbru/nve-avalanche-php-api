fc92ce6c097b3c3a42a9ddb6e7e8460bbeb8c1ba
Torstein Skulbru <serrghi@gmail.com>
2013-05-28 20:26:39 +0200

    Fixed Licence file
    	modified:   LICENSE
    	deleted:    src/AvalancheProblem.php
    	deleted:    src/AvalancheWarningDetail.php
    	deleted:    src/AvalancheWarningSimple.php
    	deleted:    src/Detail.php
    	deleted:    src/Language.php
    	deleted:    src/NveApi.php
    	deleted:    src/Region.php
    	deleted:    src/Url.php
    	deleted:    tests/NveApiTest.php
    	modified:   tests/bootstrap.php
    	modified:   tests/phpunit.xml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'AutoLoader.php';
-AutoLoader::registerDirectory('../src/');
-require_once '../src/AvalancheWarningDetail.php';
-require_once '../src/AvalancheProblem.php';
+error_reporting(E_ALL);
+
+$loader = require __DIR__.'/../src/bootstrap.php';
+$loader->add('Nve\Test', __DIR__);
